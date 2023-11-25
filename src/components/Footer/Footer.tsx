9b0f1c2cc7d2f1bcad4a68562fb264995c55495e
mmA1r <94727602+mmA1r@users.noreply.github.com>
2023-11-26 00:23:33 +0400

    main page done

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,39 @@
+import { useEffect, useRef } from 'react';
+
+import Socials from './Socials/Socials';
+
 import './footer.scss'
 
 const Footer = () => {
+    const socialsRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const socials = socialsRef.current;
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("socials_in-view");
+
+                    if (socials) {
+                        observer.unobserve(socials);
+                    }
+                }
+            });
+        });
+        if (socials) {
+            observer.observe(socials);
+        }
+    }, []);
 
     return (
         <section className={"footer"}>
-            {/*<Socials />*/}
-            {/*<Waves />*/}
+            <div 
+                ref={socialsRef} 
+                className='socials__wrapper'
+            >
+                <Socials />
+                {/*<Waves />*/}
+            </div>
         </section>
     );
 }
---
+++ src/components/Footer/Socials/Socials.tsx
@@ -0,0 +1,14 @@
+import Links from "./Links/Links";
+
+import './socials.scss';
+
+const Socials = () => {
+    return (
+        <section className={"footer_socials"}>
+            <h2>SOCIALS</h2>
+            <Links />
+        </section>
+    );
+}
+
+export default Socials;
